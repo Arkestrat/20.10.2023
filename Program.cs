72042f715a1a146c86135c4c667ea770ef2fbb90
Arkestrat <115097705+Arkestrat@users.noreply.github.com>
2023-10-19 20:01:46 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,68 +1,19 @@
-﻿////zadanie_4_stranisa_38
-//int[] vso = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-//int tarNum = 1;
-//int count = 0;
-//foreach (int num in vso)
-//{
-//    if (num == tarNum)
-//    {
-//        count++;
-//    }
-//}
-//Console.WriteLine($"Число {tarNum} встречается в последовательности {count} раз ");
+﻿//stranisa36_1b
+double e = double.Parse(Console.ReadLine());
+double sum = 0;
+while (true)
+{
+    double r = double.Parse(Console.ReadLine());
+    r = (1 / Math.Pow(2, r) + 1 / Math.Pow(3, r));
+    if (Math.Abs(r) >= e)
+    {
+        sum += r;
+    }
+    Console.WriteLine("Продольжить последовательность? Y/N");
+    if (Console.ReadLine() == "N")
+    {
+        break;
+    }
 
-////zadanie_1_stranisa_38
-//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//int sum = 0;
-//foreach (int number in sequence)
-//{
-//    sum += number;
-//}
-//Console.WriteLine($"Сумма элементов последовательности: {sum}");
-
-////zadanie_6_stranisa_38
-//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//int product = 1;
-//foreach (int number in sequence)
-//{
-//    product *= number;
-//}
-//Console.WriteLine($"Произведение элементов последовательности: {product}");
-
-////zadanie_7_stranisa_38
-//int[] sequence = { -1, 2, -3, 4, -5, 6, -7, 8, -9 };
-//int sum = 0;
-//foreach (int number in sequence)
-//{
-//    sum +=  Math.Abs(number);
-//}
-//Console.WriteLine($"Сумма элементов последовательности: {sum}");
-
-////zadanie_9_stranisa_38
-//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//int sum = 0;
-//foreach (int number in sequence)
-//{
-//    sum += number * number;
-//}
-//Console.WriteLine($"Сумма элементов последовательности: {sum}");
-
-////zadanie_10_stranisa_38
-//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//int sum = 0;
-//int sum1 = 0;
-//foreach (int number in sequence)
-//{
-//    sum += number;
-//    sum1 = sum / 9;
-//}
-//Console.WriteLine($"Среднее арифметическое последовательности: {sum1}");
-
-////zadanie_12_stranisa_38
-//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//int sum = -1;
-//foreach (int number in sequence)
-//{
-//   sum+=  number*number;
-//}
-//Console.WriteLine($"Сумма элементов последовательности: {sum}");
+}
+Console.WriteLine(sum);
